ca9106a0bc6c5831676b6f021f4e4b1592f3cf7e
Álvaro de la Mata <amgko3d@gmail.com>
2019-12-03 13:03:11 +0100

    Remove drill-override.conf hacks, moved to K8s config map to avoid rebuild for each change

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 
 usage() {
     echo "Usage:"
-    echo "        ${0}  -e ENVIRONMENT -p AWS_PROFILE"
-    echo "Example:"
-    echo "        ${0}  -e tvi -p perfil-pruebas"
+    echo "    ${0}"
 }
 
 download_release_file () {
@@ -29,65 +27,24 @@ download_release_file () {
   fi
 }
 
-MIRADA_REVISION=1
+MIRADA_REVISION=2
 IMAGE_NAME=apache-drill
 IMAGE_TAG=1.16.0-$MIRADA_REVISION
 MIRADA_UDF_VERSION=0.6
 
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
 
-case $key in
-    -e|--environment)
-    TARGET_ENVIRONMENT="$2"
-    shift ; shift ;;
-    -p|--profile)
-    AWS_PROFILE="$2"
-    shift ; shift ;;
-    -h|--help)
-    usage
-    exit 0 ;;  
-    *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
-    ;;
-esac
-done
-set -- "${POSITIONAL[@]}" # restore positional parameters
-
-if [[ -z "$TARGET_ENVIRONMENT" ]] || [[ -z "$AWS_PROFILE" ]]; then
-    usage
-    exit 127
-fi
-
-if [[ "$TARGET_ENVIRONMENT" == "tvi" ]]; then
-    AWS_REGION=us-east-1
-    ECR_URN=606039442951.dkr.ecr.$AWS_REGION.amazonaws.com
-elif [[ "$TARGET_ENVIRONMENT" == "dgtv" ]]; then
-    AWS_REGION=us-east-1
-    ECR_URN=258349028160.dkr.ecr.$AWS_REGION.amazonaws.com/mirada/logiq
-elif [[ "$TARGET_ENVIRONMENT" == "one" ]] || [[ "$TARGET_ENVIRONMENT" == "viya" ]]; then
-    AWS_REGION=us-east-1
-    ECR_URN=891435880877.dkr.ecr.$AWS_REGION.amazonaws.com/mirada/logiq
-elif [[ "$TARGET_ENVIRONMENT" == "izzi" ]]; then
-    AWS_REGION=us-west-2
-    ECR_URN=628220405432.dkr.ecr.$AWS_REGION.amazonaws.com
-else
-    echo "ERROR: Invalid target environment. Valid are: tvi, dgtv, viya, one, izzi"
-    usage
-    exit 127
-fi
+ECR_URN=docker-registry.mirada.lab:5000/logiq
 echo Downloading binaries...
+
 
 download_release_file tvmetrix-drill-udf $MIRADA_UDF_VERSION tvmetrix-drill-udf-$MIRADA_UDF_VERSION.jar
 download_release_file tvmetrix-drill-udf $MIRADA_UDF_VERSION tvmetrix-drill-udf-${MIRADA_UDF_VERSION}-sources.jar
+
 
 docker build \
     --build-arg MIRADA_UDF_VERSION=$MIRADA_UDF_VERSION \
     -t $IMAGE_NAME:$IMAGE_TAG .
 
-$(aws ecr get-login --no-include-email --region $AWS_REGION --profile $AWS_PROFILE)
+
 docker tag $IMAGE_NAME:$IMAGE_TAG $ECR_URN/$IMAGE_NAME:$IMAGE_TAG
 docker push $ECR_URN/$IMAGE_NAME:$IMAGE_TAG
--- drill-env.sh
+++ drill-env.sh
@@ -1,4 +1,3 @@
-export DRILL_BUFFER_SIZE=${DRILL_BUFFER_SIZE:=100}
 export DRILL_LOG_LEVEL=${DRILL_LOG_LEVEL:=info}
 export DRILL_JAVA_LIB_PATH="/opt/hadoop-$HADOOP_VERSION/lib/native"
 
@@ -17,10 +16,3 @@ fi
 sed -i "s|S3A_CONNECTION_MAXIMUM|$S3A_CONNECTION_MAXIMUM|" $drill_conf/core-site.xml
 sed -i "s|S3A_ENDPOINT|$S3A_ENDPOINT|" $drill_conf/core-site.xml
 sed -i "s|DRILL_LOG_LEVEL|$DRILL_LOG_LEVEL|" $drill_conf/logback.xml
-
-# Drill overrides
-drill_override_conf=$drill_conf/drill-override.conf
-echo "drill.exec.cluster-id: \"$DRILL_CLUSTER\"" > $drill_override_conf
-echo "drill.exec.zk.connect: \"$ZOOKEEPER\"" >> $drill_override_conf
-echo "drill.exec.buffer.size: $DRILL_BUFFER_SIZE" >> $drill_override_conf
-
