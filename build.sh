65e0eb0cb8a22b4e7f7d01a1dc72df119d2e41f1
Álvaro de la Mata <amgko3d@gmail.com>
2020-05-27 16:31:48 +0200

    Upgrade UDFs to 0.12 + bump version 1.16.0-8

--- build.sh
+++ build.sh
@@ -27,10 +27,10 @@ download_release_file () {
   fi
 }
 
-MIRADA_REVISION=7
+MIRADA_REVISION=8
 IMAGE_NAME=apache-drill
 IMAGE_TAG=1.16.0-$MIRADA_REVISION
-MIRADA_UDF_VERSION=0.11
+MIRADA_UDF_VERSION=0.12
 
 
 ECR_URN=docker-registry.mirada.lab:5000/logiq
